4d5f3a3ebbfd0f505b23efd0dee81e7e24dc81e1
Nicholas Hatch <nwhatch@outlook.com>
2025-12-01 20:14:25 -0500

    Fix main.go to properly call root command Execute()

--- cmd/aumc/main.go
+++ cmd/aumc/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	"github.com/AutismUp/aumc_tools/cmd"
 )
 
 func main() {
-	fmt.Println("AUMC - Autism Up Minecraft Tool")
-	fmt.Printf("Cobra imported: %T\n", &cobra.Command{})
-	fmt.Printf("Viper loaded: %v\n", viper.New() != nil)
-	os.Exit(0)
+	cmd.Execute()
 }
